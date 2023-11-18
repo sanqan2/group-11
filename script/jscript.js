aab1102b284db139a04032d37ae280ce27056d63
sanqan2 <esandqvist04@gmail.com>
2023-11-18 15:30:23 +0100

    header

--- script/jscript.js
+++ script/jscript.js
@@ -1,8 +1,15 @@
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
+
+const menu = document.querySelector(".menu");
+const navmenu = document.querySelector(".nav-menu");
+menu.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  navmenu.classList.toggle("active");
+});
